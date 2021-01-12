56cd7c1b3d51ada18f238e4661b8aa565e55d7ad
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-12 17:49:06 +0400

    Add GET all users

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,21 @@ const db = require('../db');
 const express = require('express');
 const usersRouter = new express.Router();
 
+usersRouter.get('/', async (req, res) => {
+  const {rows} = await db.query('SELECT * FROM users');
+  if (rows) {
+    res.status(200).send(rows);
+  } else {
+    res.status(404).send();
+  }
+});
+
 usersRouter.get('/:id', async (req, res) => {
   const id = Number(req.params.id);
   if (id) {
     const {rows} = await db.query('SELECT * FROM users WHERE id = $1', [id]);
     if (rows[0]) {
-      res.send(rows[0]);
+      res.status(200).send(rows[0]);
     } else {
       res.status(404).send();
     }
